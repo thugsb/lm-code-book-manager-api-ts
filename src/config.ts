47a4adf9f11340c0019072560bf081afc5ec8fa5
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-07 13:54:31 +0000

    Feat: Connect to an actual DB

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,25 @@
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+	path: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+});
+
+const {
+	PORT,
+	DB_NAME,
+	DB_USERNAME,
+	DB_PASSWORD,
+	DB_HOST,
+	DB_DIALECT,
+	DB_PORT,
+} = process.env;
 
 export const CONFIG = {
-	port: process.env.PORT ?? 3000,
-	dbName: process.env.DB_NAME ?? "sqlite::memory:",
-	dbUserName: process.env.DB_USERNAME ?? "",
-	dbPassword: process.env.DB_PASSWORD ?? "",
-	dbHost: process.env.DB_HOST ?? "localhost",
-	dbDialect: process.env.DB_DIALECT ?? "sqlite",
+	port: PORT ?? 3000,
+	dbName: DB_NAME ?? "sqlite::memory:",
+	dbUserName: DB_USERNAME ?? "",
+	dbPassword: DB_PASSWORD ?? "",
+	dbHost: DB_HOST ?? "localhost",
+	dbDialect: DB_DIALECT ?? "sqlite",
+	dbPort: DB_PORT ?? "5432",
 } as const;
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,28 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
 
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+export let sequelize = new Sequelize("sqlite::memory:");
+
+const {
+	DB_NAME,
+	DB_USERNAME,
+	DB_PASSWORD,
+	DB_HOST,
+	DB_DIALECT,
+	DB_PORT,
+	NODE_ENV,
+} = process.env;
+
+if (NODE_ENV !== "test") {
+	sequelize = new Sequelize(
+		DB_NAME ?? "MISSING_DB_NAME_CONFIG",
+
+		DB_USERNAME ?? "MISSING_DB_USERNAME_CONFIG",
+
+		DB_PASSWORD ?? "MISSING_DB_PASSWORD_CONFIG",
+		{
+			host: DB_HOST ?? "MISSING_DB_HOST_CONFIG",
+			port: parseInt(DB_PORT as string) ?? "MISSING_DB_PORT_CONFIG",
+			dialect: (DB_DIALECT as Dialect) ?? "postgres",
+		}
+	);
+}
